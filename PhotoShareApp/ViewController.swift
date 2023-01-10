c14b4b84dab3ba74093fb9859cc7c102faf6c14c
Emre Sahin <mehmetemre9906@gmail.com>
2023-01-10 23:17:10 +0300

    updated

--- PhotoShareApp/ViewController.swift
+++ PhotoShareApp/ViewController.swift
@@ -3,6 +3,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
