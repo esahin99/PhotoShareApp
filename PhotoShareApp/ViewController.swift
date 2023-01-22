274e389a0d642a7b77097cc56d58389c59abe72c
Emre Sahin <mehmetemre9906@gmail.com>
2023-01-22 13:22:37 +0300

    updated

--- PhotoShareApp/ViewController.swift
+++ PhotoShareApp/ViewController.swift
@@ -9,11 +9,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func loginButton(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != ""{
+            //User Login
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){ (AuthDataResult, error) in
+                if error != nil{
+                    self.errorMessage(titleInput: "Error", messageInput: error!.localizedDescription)
+                }
+                else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }
+        else{
+            errorMessage(titleInput: "Error!", messageInput: "Please Enter Name and Password!")
+        }
     }
     @IBAction func registerButton(_ sender: Any) {
         if emailTextField.text != "" && passwordTextField.text != ""{
-            //Kayıt olma işlemi
-            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authDataResult, error in
+            //User Register
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authDataResult, error) in
                 if error != nil{
                     self.errorMessage(titleInput: "Error!", messageInput: error!.localizedDescription)
                 }
